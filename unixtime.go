eea7f26d9afbcacef8d1915507a1d6042bf76e5b
Yinyin L <yinyinl@gmail.com>
2020-11-01 01:31:51 +0800

    add cast function

--- unixtime.go
+++ unixtime.go
@@ -6,3 +6,9 @@ import (
 
 // UnixTime is alias of time.Time.
 type UnixTime time.Time
+
+// Time cast value into time.Time.
+func (v UnixTime) Time() (t time.Time) {
+	t = (time.Time)(v)
+	return
+}
